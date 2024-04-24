0cd691a12bf6bbb060ec481229137d71fece3622
nikhilchame03 <119056424+nikhilchame03@users.noreply.github.com>
2024-04-24 21:10:04 +0530

    Update main.py

--- recomendation-sys-web/main.py
+++ recomendation-sys-web/main.py
@@ -1,75 +1,45 @@
-import streamlit as st
-import os
-from PIL import Image
-import numpy as np
 import pickle
 import tensorflow
+import numpy as np
 from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.resnet50 import ResNet50,preprocess_input
 from tensorflow.keras.layers import GlobalMaxPooling2D
-from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input
-from sklearn.neighbors import NearestNeighbors
 from numpy.linalg import norm
+from sklearn.neighbors import NearestNeighbors
+import cv2
 
-feature_list = np.array(pickle.load(open('embeddings.pkl', 'rb')))
-filenames = pickle.load(open('filenames.pkl', 'rb'))
 
-model = ResNet50(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
+
+feature_list = pickle.load(open('embeddings.pkl','rb'))
+filenames = pickle.load(open('filenames.pkl','rb'))
+#print(np.array(feature_list).shape)
+
+model = ResNet50(weights='imagenet',include_top=False,input_shape=(224,224,3))
 model.trainable = False
 
 model = tensorflow.keras.Sequential([
     model,
     GlobalMaxPooling2D()
-])
-
-st.title('Fashion Recommender System')
+ ])
 
 
-def save_uploaded_file(uploaded_file):
-    try:
-        with open(os.path.join('uploads', uploaded_file.name), 'wb') as f:
-            f.write(uploaded_file.getbuffer())
-        return 1
-    except:
-        return 0
+img = image.load_img('samples/2300.jpg',target_size=(224,224))
+img_array = image.img_to_array(img)
+expanded_img_array = np.expand_dims(img_array, axis=0)
+preprocessed_img = preprocess_input(expanded_img_array)
+result = model.predict(preprocessed_img).flatten()
+normalized_result = result / norm(result)
 
 
-def feature_extraction(img_path, model):
-    img = image.load_img(img_path, target_size=(224, 224))
-    img_array = image.img_to_array(img)
-    expanded_img_array = np.expand_dims(img_array, axis=0)
-    preprocessed_img = preprocess_input(expanded_img_array)
-    result = model.predict(preprocessed_img).flatten()
-    normalized_result = result / norm(result)
+neighbors = NearestNeighbors(n_neighbors=6,algorithm='brute',metric='euclidean')
+neighbors.fit(feature_list)
 
-    return normalized_result
+distances,indices = neighbors.kneighbors([normalized_result])
+
+print(indices)
 
 
-def recommend(features, feature_list):
-    neighbors = NearestNeighbors(n_neighbors=6, algorithm='brute', metric='euclidean')
-    neighbors.fit(feature_list)
-
-    distances, indices = neighbors.kneighbors([features])
-
-    return indices
-
-
-# steps
-# file upload -> save
-uploaded_file = st.file_uploader("Choose an image")
-if uploaded_file is not None:
-    if save_uploaded_file(uploaded_file):
-        # display the file
-        display_image = Image.open(uploaded_file)
-        st.image(display_image)
-        # feature extract
-        features = feature_extraction(os.path.join("uploads", uploaded_file.name), model)
-        # st.text(features)
-        # recommendation
-        indices = recommend(features, feature_list)
-        # show
-        cols = st.columns(5)
-
-        for i, col in enumerate(cols):
-            col.image(filenames[indices[0][i]])
-    else:
-        st.header("Some error occurred in file upload")
+for file in indices[0][1:6]:
+    temp_img = cv2.imread(filenames[file])
+    cv2.imshow('output',cv2.resize(temp_img,(512,512)))
+    cv2.waitKey(0)
